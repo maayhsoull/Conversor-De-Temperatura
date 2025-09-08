be5e93e314c7614be0fd044f03861e5347e7c8a5
MAYARA MARTINS DE ANDRADE <25203738@CFP127ALUNO.LOCAL>
2025-09-08 11:20:36 -0300

    Realizando Clean Code e mudando nomes de variáveis.

--- src/Conversor.java
+++ src/Conversor.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Conversor {
 
-    private double temperatura, fahrenheit, kelvin;
+    private double temperaturaEmCelcius, temperaturaEmFahrenheit, temperaturaEmKelvin;
 
     public void obterDados(){
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite a temperatura em Celcius: ");
-        temperatura = sc.nextDouble();
+        temperaturaEmCelcius = sc.nextDouble();
         calculoTemperatura();
         exibirResultado();
 
@@ -17,13 +17,13 @@ public class Conversor {
 
     private void calculoTemperatura(){
 
-        fahrenheit = (temperatura * 1.8) + 32;
+        temperaturaEmFahrenheit = (temperaturaEmCelcius * 1.8) + 32;
 
-        kelvin = temperatura + 273.15;
+        temperaturaEmKelvin = temperaturaEmCelcius + 273.15;
 
     }
 
     private void exibirResultado(){
-        System.out.printf("%.2fºC equivalem a:\n%.2fºF (Fahrenheit) \n%.2fºK (Kelvin)", temperatura, fahrenheit, kelvin);
+        System.out.printf("%.2fºC equivalem a:\n%.2fºF (Fahrenheit) \n%.2fºK (Kelvin)", temperaturaEmCelcius, temperaturaEmFahrenheit, temperaturaEmKelvin);
     }
 }
