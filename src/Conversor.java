b944167c385c488e7879b8f6e536807b8a41160a
MAYARA MARTINS DE ANDRADE <25203738@CFP127ALUNO.LOCAL>
2025-09-08 08:28:06 -0300

    Conversor de temperatura em java

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Conversor {
+
+    private double temperatura, fahrenheit, kelvin;
+
+    public void obterDados(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a temperatura em Celcius: ");
+        temperatura = sc.nextDouble();
+        calculoTemperatura();
+        exibirResultado();
+
+    }
+
+    private void calculoTemperatura(){
+
+        fahrenheit = (temperatura * 1.8) + 32;
+
+        kelvin = temperatura + 273.15;
+
+    }
+
+    private void exibirResultado(){
+        System.out.printf("%.2fºC equivalem a:\n%.2fºF (Fahrenheit) \n%.2fºK (Kelvin)", temperatura, fahrenheit, kelvin);
+    }
 }
